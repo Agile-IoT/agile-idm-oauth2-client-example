8020f32166a81dd6b33a774eabf86ba0a5a0e7bb
david <nopbyte>
2016-12-01 11:30:54 +0100

    improve flow of website (redirect automatically and include logout function)

--- routes/index.js
+++ routes/index.js
@@ -17,8 +17,25 @@ function router(conf) {
   });
 
   /* index */
-  router.route('/').get(function(req,res){
-    res.render("index");
+  //this route uses the login middleware to ensure there is a user logged in, and then shows the authenticated website
+  //in this way, when a user is authenticated and goes to the home page, he sees the authenticated site, if not he is redirected.
+  //but there is no "landing" page to click on "authenticate with IDM"
+  //If you want to have users to land in only one site, regardless of whether they are authenticated or not use this
+  //
+  // router.route('/').get(function(req,res){
+  //  res.render("index");
+  //});
+  //
+
+  router.route('/').get( login.ensureLoggedIn('/auth/example/'), function(req,res){
+    res.render("authenticated");
+  });
+
+
+  router.route('/logout').get( login.ensureLoggedIn('/auth/example/'), function(req,res){
+    req.logout();
+    //give the user the oportunity to login again
+    res.render('index');
   });
 
 
