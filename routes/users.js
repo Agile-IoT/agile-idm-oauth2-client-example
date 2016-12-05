eb9db2049577e04caa4255c4948cb56eeb670421
david <nopbyte>
2016-12-05 09:51:59 +0100

    endpoints to create and delete users work now :)

--- routes/users.js
+++ routes/users.js
@@ -50,7 +50,7 @@ function router(conf, idm_conf, router) {
             res.render('result',{"result":[{"label":"error","value":"unexpected result from IDM  endpoint "+error}],"action":action});
           }
         } else if (!error) {
-            res.render('result',{"result":[{"label":"error","value":"unexpected status code from IDM  endpoint :"+res.statusCode}],"action":action});
+            res.render('result',{"result":[{"label":"error","value":"unexpected status code from IDM  endpoint :"+response.statusCode+ "error:"+  response.body}],"action":action});
         } else {
           res.render('result',{"result":[{"label":"error","value":"unexpected result from IDM  endpoint "+error}],"action":action});
         }
@@ -67,7 +67,46 @@ function router(conf, idm_conf, router) {
 
 
   router.route('/create_user').post( login.ensureLoggedIn('/auth/example/'), function (req, res) {
-    res.render('result',{"result":[{"label":"stuff","value":"thing"}],"action":"create user"});
+    var action = "create user";
+    //first we read the token
+    tokens.find(req.user.id, function(error, accesstoken){
+      var user = {
+                     "auth_type": req.body.auth_type,
+                      "user_name": req.body.user_name
+      };
+      //build http options
+      var options = {
+        url: url + '/user/',
+        body: JSON.stringify(user),
+        headers: {
+          'Authorization': 'bearer ' + accesstoken,
+          'User-Agent': 'user-agent',
+          'Content-type': 'application/json'
+        }
+      };
+      //send request
+      /*
+       the render view expects  an object called result with the format:
+        {"result":[{"label":"label1","value":"value1"},{"label":"label2","value":"value2"},...],"action":"type of action"};
+       so here we build it properly with utils and passing the action type.
+      */
+      request.post(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          try {
+            var result = JSON.parse(body);
+            res.render('result',{"result":util.formatOutput(result),"action":action});
+
+          } catch (error) {
+            res.render('result',{"result":[{"label":"error","value":"unexpected result from IDM  endpoint "+error}],"action":action});
+          }
+        } else if (!error) {
+            res.render('result',{"result":[{"label":"error","value":"unexpected status code from IDM  endpoint :"+response.statusCode+ "response:"+ response.body}],"action":action});
+        } else {
+          res.render('result',{"result":[{"label":"error","value":"unexpected result from IDM  endpoint "+error}],"action":action});
+        }
+      });
+    });
+
   });
 
   /*
@@ -77,9 +116,45 @@ function router(conf, idm_conf, router) {
     res.render("find_user",{"action":"delete"});
   });
 
-
+  //deleting the user...
   router.route('/delete_user').post( login.ensureLoggedIn('/auth/example/'), function (req, res) {
-    res.render('result',{"result":[{"label":"stuff","value":"thing"}],"action":"delete "});
+    var action = "delete user";
+    //first we read the token
+    tokens.find(req.user.id, function(error, accesstoken){
+      var query = querystring.stringify({
+                      "auth_type": req.body.auth_type,
+                      "user_name": req.body.user_name
+      });
+      //build http options
+      var options = {
+        url: url + '/user/?'+query,
+        headers: {
+          'Authorization': 'bearer ' + accesstoken,
+          'User-Agent': 'user-agent',
+          'Content-type': 'application/json'
+        }
+      };
+      //send request
+      /*
+       the render view expects  an object called result with the format:
+        {"result":[{"label":"label1","value":"value1"},{"label":"label2","value":"value2"},...],"action":"type of action"};
+       so here we build it properly with utils and passing the action type.
+      */
+      request.delete(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          try {
+            res.render('result',{"result":[{"value":"user deleted successfully","label":"result"}],"action":action});
+
+          } catch (error) {
+            res.render('result',{"result":[{"label":"error","value":"unexpected result from IDM  endpoint "+error}],"action":action});
+          }
+        } else if (!error) {
+            res.render('result',{"result":[{"label":"error","value":"unexpected status code from IDM  endpoint :"+response.statusCode + "error:"+  response.body}],"action":action});
+        } else {
+          res.render('result',{"result":[{"label":"error","value":"unexpected result from IDM  endpoint "+error}],"action":action});
+        }
+      });
+    });
   });
   return router;
 }
