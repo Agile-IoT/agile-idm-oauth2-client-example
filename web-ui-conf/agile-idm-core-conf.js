793d0aa7a9df3c5652bde6c7293bca3c9f64f326
david <nopbyte@users.noreply.github.com>
2017-08-17 17:11:00 +0200

    move to device from sensor as entity type. this is compatible with releases after v1.0.2 of agile-security

--- web-ui-conf/agile-idm-core-conf.js
+++ web-ui-conf/agile-idm-core-conf.js
@@ -3,72 +3,129 @@ module.exports = {
   "storage": {
     "dbName": "database_"
   },
+  upfront_storage: {
+    module_name: "agile-upfront-leveldb",
+    type: "external",
+    dbName: "./pap-database",
+    collection: "policies",
+  },
   "policies": {
-    "dbName": "./policies.json",
     "create_entity_policy": [
       // actions of an actor are not restricted a priori
       {
-        target: {
-          type: "any"
-        }
-      }, {
-        source: {
-          type: "any"
-        }
-      }
-    ],
-    "top_level_policy": [
-      // all properties can be read by everyone
-      {
-        target: {
-          type: "any"
-        }
+        op: "write"
       },
-      // all properties can only be changed by the owner of the entity
       {
-        source: {
-          type: "user"
-        },
-        locks: [{
-          lock: "isOwner"
-        }]
-      }, {
-        source: {
-          type: "user"
-        },
-        locks: [{
-          lock: "attrEq",
-          args: ["role", "admin"]
-        }]
+        op: "read"
       }
     ],
+    "top_level_policy": {
+      flows: [
+        // all properties can be read by everyone
+        {
+          op: "read"
+        },
+        // all properties can only be changed by the owner of the entity
+        {
+          op: "write",
+          locks: [{
+            lock: "hasType",
+            args: ["/user"]
+          }, {
+            lock: "isOwner"
+          }]
+        },
+        {
+          op: "write",
+          locks: [{
+            lock: "hasType",
+            args: ["/user"]
+          }, {
+            lock: "attrEq",
+            args: ["role", "admin"]
+          }]
+        }
+
+      ],
+      //specify what should happen if the policy does not comply
+      actions: {
+        "read": [{
+          action: "delete"
+        }]
+      }
+    },
     "attribute_level_policies": {
       "user": {
-        "password": [
-          // the property can only be read by the user itself
-          {
-            target: {
-              type: "user"
-            },
+        //policies for actions executed for this entity (owner or admin only)
+        "actions": [{
+            op: "read",
             locks: [{
-              lock: "isOwner"
-            }]
-          },
-          // the property can be set by the user itself and
-          {
-            source: {
-              type: "user"
-            },
-            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
               lock: "isOwner"
             }]
           },
           // by all users with role admin
           {
-            source: {
-              type: "user"
-            },
+            op: "read",
             locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "attrEq",
+              args: ["role", "admin"]
+            }]
+          },
+          {
+            op: "write",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "isOwner"
+            }]
+          },
+          // by all users with role admin
+          {
+            op: "write",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "attrEq",
+              args: ["role", "admin"]
+            }]
+          }
+        ],
+        "password": [
+          // the property can only be read by the user itself
+          {
+            op: "read",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "isOwner"
+            }]
+          }
+          // the property can be set by the user itself and
+          , {
+            op: "write",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "isOwner"
+            }]
+          },
+          // by all users with role admin
+          {
+            op: "write",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
               lock: "attrEq",
               args: ["role", "admin"]
             }]
@@ -77,72 +134,250 @@ module.exports = {
         "role": [
           // can be read by everyone
           {
-            target: {
-              type: "any"
-            }
+            op: "read"
           },
           // can only be changed by users with role admin
           {
-            source: {
-              type: "user"
-            },
+            op: "write",
             locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "attrEq",
+              args: ["role", "admin"]
+            }]
+          }
+        ],
+        "credentials": [
+          // the property can only be read by the user itself
+          {
+            op: "read"
+          },
+          // the property can be set by the user itself and
+          {
+            op: "write",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "isOwner"
+            }]
+          },
+          {
+            op: "write",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "attrEq",
+              args: ["role", "admin"]
+            }]
+          }
+        ],
+        "credentials.dropbox": [{
+            op: "read",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "isOwner"
+            }]
+          },
+          {
+            op: "write",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "isOwner"
+            }]
+          },
+          // by all users with role admin
+          {
+            op: "write",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
               lock: "attrEq",
               args: ["role", "admin"]
             }]
           }
         ]
       },
-      "sensor": {
+      "gateway": {
+        //policies for actions executed for this entity (owner or admin only)
+        "actions": [{
+            op: "read",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "isOwner"
+            }]
+          },
+          // by all users with role admin
+          {
+            op: "read",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "attrEq",
+              args: ["role", "admin"]
+            }]
+          },
+          {
+            op: "write",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "isOwner"
+            }]
+          },
+          // by all users with role admin
+          {
+            op: "write",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "attrEq",
+              args: ["role", "admin"]
+            }]
+          }
+        ]
+      },
+      "device": {
+        //policies for actions executed for this entity (owner or admin only)
+        "actions": [{
+            op: "read",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "isOwner"
+            }]
+          },
+          // by all users with role admin
+          {
+            op: "read",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "attrEq",
+              args: ["role", "admin"]
+            }]
+          },
+          {
+            op: "write",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "isOwner"
+            }]
+          },
+          // by all users with role admin
+          {
+            op: "write",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "attrEq",
+              args: ["role", "admin"]
+            }]
+          }
+        ],
         "credentials": [
           // the property can only be read by the user itself
           {
-            target: {
-              type: "user"
-            },
+            op: "read",
             locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
               lock: "isOwner"
             }]
           },
           // the property can be set by the user itself and
           {
-            source: {
-              type: "user"
-            },
+            op: "write",
             locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
               lock: "isOwner"
+            }]
+          }
+        ],
+        "credentials.dropbox": [{
+            op: "read",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "isOwner"
+            }]
+          },
+          {
+            op: "write",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "isOwner"
+            }]
+          },
+          // by all users with role admin
+          {
+            op: "write",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "attrEq",
+              args: ["role", "admin"]
             }]
           }
         ]
       }
-
     }
   },
+  "forbidden-attribute-names": [
+    'id',
+    'type',
+    'owner',
+    'groups',
+    'entities',
+    'actions'
+  ],
   "schema-validation": [{
-    "id": "/sensor",
+    "id": "/device",
+    "additionalProperties": false,
     "type": "object",
     "properties": {
       "name": {
         "type": "string"
       },
       "credentials": {
-        "type": "array",
-        "items": {
-          "type": "object",
-          "properties": {
-            "system": {
-              "type": "string"
-            },
-            "value": {
-              "type": "string"
-            }
+        "type": "object",
+        "additionalProperties": true,
+        "properties": {
+          "dropbox": {
+            "type": "string"
           }
         }
       }
+
     },
     "required": ["name"]
   }, {
     "id": "/user",
+    "additionalProperties": false,
     "type": "object",
     "properties": {
       "user_name": {
@@ -154,14 +389,15 @@ module.exports = {
       "password": {
         "type": "string"
       },
-      "role":{
-        "type":"string"
+      "role": {
+        "type": "string"
       }
     },
     "required": ["user_name", "auth_type"]
   }, {
     "id": "/client",
     "type": "object",
+    "additionalProperties": false,
     "properties": {
       "name": {
         "type": "string"
@@ -173,6 +409,35 @@ module.exports = {
         "type": "string"
       }
     },
-    "required": ["name", "clientSecret", "redirectURI"]
-  }]
+    "required": ["name"]
+  }, {
+    "id": "/gateway",
+    "type": "object",
+    "additionalProperties": false,
+    "properties": {
+      "name": {
+        "type": "string"
+      }
+    },
+    "required": ["name"]
+  }],
+  "configure_on_boot": {
+    "user": [{
+      "user_name": "bob",
+      "auth_type": "agile-local",
+      "role": "admin",
+      "password": "secret"
+    }],
+    "client": [{
+      "id": "MyAgileClient2",
+      "name": "MyAgileClient2",
+      "clientSecret": "Ultrasecretstuff",
+      "redirectURI": "http://localhost:3002/auth/example/callback"
+    }],
+    "gateway": [{
+      "id": "self",
+      "name": "local gateway"
+    }]
+  }
+
 };
